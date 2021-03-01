ad83ef29eb9af013f28dff0da8099c5f8c64d202
Federico <federicofigini90@gmail.com>
2021-03-01 18:34:06 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,40 @@
-var eta = parseInt(prompt("Inserisci la tua eta"))
-var km = parseInt(prompt("Inserisci quanti KM vuoi percorrere"))
+var ageEl = document.getElementById("age");
+var kmEl = document.getElementById("km");
+var intEl = document.getElementById("int");
+var overEl = document.getElementById("over");
+var underEl = document.getElementById("under");
+var discountEl = document.getElementById("discount");
 
+var eta = parseInt(prompt("Inserisci la tua eta"));
+var km = parseInt(prompt("Inserisci quanti KM vuoi percorrere"));
 
-document.getElementById("age").innerHTML = eta
-document.getElementById("km").innerHTML = km
+ageEl.innerHTML = "#";
+kmEl.innerHTML = "#";
 
-
-
-var int = km * 0.21
-document.getElementById("int").innerHTML = int + " €"
-
-
-if (eta > 65) {
-    var over = int - (int * 0.4)
-    document.getElementById("over").innerHTML = over + " €"
-    document.getElementById("discount").innerHTML = "HAI DIRITTO AD UNO SCONTO OVER 65 DI: " + int * 0.4 + " €"
-} else if (eta < 18) {
-    var under = int - (int * 0.2)
-    document.getElementById("under").innerHTML = under + " €"
-    document.getElementById("discount").innerHTML = "HAI DIRITTO AD UNO SCONTO UNDER 18 DI: " + int * 0.2 + " €"
-
+if (isNaN(eta) && isNaN(km)) {
+    discountEl.innerHTML = "DEVI INSERIRE UN ETA E I KM";
+} else if (isNaN(eta)) {
+    discountEl.innerHTML = "DEVI INSERIRE UN ETA";
+    kmEl.innerHTML = km;
+} else if (isNaN(km)) {
+    discountEl.innerHTML = "DEVI INSERIRE I KM";
+    ageEl.innerHTML = eta;
 } else {
-    document.getElementById("discount").innerHTML = "NON HAI DIRITTO A NESSUNO SCONTO"
+    ageEl.innerHTML = eta;
+    kmEl.innerHTML = km;
+
+    var int = km * 0.21;
+    document.getElementById("int").innerHTML = int + " €";
+
+    if (eta > 65) {
+        var over = int - (int * 0.4);
+        document.getElementById("over").innerHTML = over + " €";
+        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO OVER 65 DI: " + int * 0.4 + " €";
+    } else if (eta < 18) {
+        var under = int - (int * 0.2);
+        document.getElementById("under").innerHTML = under + " €";
+        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO UNDER 18 DI: " + int * 0.2 + " €";
+    } else {
+        discountEl.innerHTML = "NON HAI DIRITTO A NESSUNO SCONTO";
+    }
 }
