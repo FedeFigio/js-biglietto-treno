e03f3c192865de8c3cc0a9f31f7dd9616496249b
Federico <federicofigini90@gmail.com>
2021-03-01 18:46:48 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,16 +24,16 @@ if (isNaN(eta) && isNaN(km)) {
     kmEl.innerHTML = km;
 
     var int = km * 0.21;
-    document.getElementById("int").innerHTML = int + " €";
+    intEl.innerHTML = int.toFixed(2) + " €";
 
     if (eta > 65) {
-        var over = int - (int * 0.4);
-        document.getElementById("over").innerHTML = over + " €";
-        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO OVER 65 DI: " + int * 0.4 + " €";
+        var over = (int - (int * 0.4)).toFixed(2);
+        overEl.innerHTML = over + " €";
+        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO OVER 65 DI: " + (int * 0.4).toFixed(2) + " €";
     } else if (eta < 18) {
-        var under = int - (int * 0.2);
-        document.getElementById("under").innerHTML = under + " €";
-        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO UNDER 18 DI: " + int * 0.2 + " €";
+        var under = (int - (int * 0.2)).toFixed(2);
+        underEl.innerHTML = under + " €";
+        discountEl.innerHTML = "HAI DIRITTO AD UNO SCONTO UNDER 18 DI: " + (int * 0.2).toFixed(2) + " €";
     } else {
         discountEl.innerHTML = "NON HAI DIRITTO A NESSUNO SCONTO";
     }
